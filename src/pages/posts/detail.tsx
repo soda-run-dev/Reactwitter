fbe527c6ab7bd41b87900b0f185f4236a63a9955
soda-run-dev <soda.run.dev@gmail.com>
2024-02-01 21:24:31 +0900

    ✨ 댓글 기능 구현

--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -1,4 +1,4 @@
-import { doc, getDoc } from "firebase/firestore";
+import { doc, onSnapshot } from "firebase/firestore";
 
 import Loader from "components/loader/Loader";
 import PostBox from "components/posts/PostBox";
@@ -7,6 +7,8 @@ import { PostProps } from "pages/home";
 import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+import CommentBox, { CommentProps } from "components/comments/CommentBox";
+import CommentForm from "components/comments/CommentForm";
 import PostHeader from "components/posts/PostHeader";
 
 export default function PostDetail() {
@@ -16,9 +18,9 @@ export default function PostDetail() {
   const getPost = useCallback(async () => {
     if (params.id) {
       const docRef = doc(db, "posts", params.id);
-      const docSnap = await getDoc(docRef);
-
-      setPost({ ...(docSnap?.data() as PostProps), id: docSnap?.id });
+      onSnapshot(docRef, (doc) => {
+        setPost({ ...(doc?.data() as PostProps), id: doc.id });
+      });
     }
   }, [params.id]);
 
@@ -29,7 +31,20 @@ export default function PostDetail() {
   return (
     <div className="post">
       <PostHeader />
-      {post ? <PostBox post={post} /> : <Loader />}
+      {post ? (
+        <>
+          <PostBox post={post} />
+          <CommentForm post={post} />
+          {post?.comments
+            ?.slice(0)
+            ?.reverse()
+            ?.map((data: CommentProps, index: number) => (
+              <CommentBox data={data} key={index} post={post} />
+            ))}
+        </>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 }
